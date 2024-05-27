d6c7c70c605685f527b01403f7078927a7eea5ff
Harshs0891 <harshs0891@gmail.com>
2024-05-27 11:21:11 +0530

    added edit and delete expense feature

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import ExpenseForm
+from .models import Expense
 # Create your views here.
 def index(request):
     if request.method=='POST':
@@ -7,6 +8,27 @@ def index(request):
         if expense.is_valid():
             expense.save()
 
+    
+    expenses = Expense.objects.all()
 
     expense_form = ExpenseForm()
-    return render(request,'myapp/index.html',{'expense_form':expense_form})
+    return render(request,'myapp/index.html',{'expense_form':expense_form,'expenses':expenses})
+
+
+def edit(request,id):
+    expense = Expense.objects.get(id=id)
+    expense_form = ExpenseForm(instance=expense)
+
+    if request.method == 'POST':
+        expense = Expense.objects.get(id=id)
+        form = ExpenseForm(request.POST,instance=expense)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    return render(request,'myapp/edit.html',{'expense_form':expense_form})
+
+def delete(request,id):
+    if request.method == 'POST' and 'delete' in request.POST:
+        expense = Expense.objects.get(id=id)
+        expense.delete()
+    return redirect('index')
